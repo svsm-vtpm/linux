f863bce0178202fd256f27d7e5968457eb5e5303
Tom Lendacky <thomas.lendacky@amd.com>
2019-11-13 16:55:56 -0600

    x86/traps: Add SEV-ES guest MSR read/write support
    RDMSR and WRMSR within an SEV-ES guest will generate a non-automatic exit
    (NAE) event for an MSR_PROT event that results in a #VC.
    
    To support MSR access within an SEV-ES guest, the instruction must parsed
    in order to determine whether it is an MSR read or an MSR write. The
    appropriate register state is set in the GHCB and VMGEXIT processing for
    an MSR_PROT event is performed as documented in the GHCB specification.
    
    Signed-off-by: Tom Lendacky <thomas.lendacky@amd.com>

--- arch/x86/mm/mem_encrypt_vc.c
+++ arch/x86/mm/mem_encrypt_vc.c
@@ -462,6 +462,48 @@ static int vmg_ioio(struct ghcb *ghcb, unsigned long ghcb_pa,
 	return 0;
 }
 
+static int vmg_msr(struct ghcb *ghcb, unsigned long ghcb_pa,
+		   struct pt_regs *regs, struct insn *insn)
+{
+	u64 exit_info_1 = 0;
+	int ret;
+
+	switch (insn->opcode.bytes[1]) {
+	case 0x30:	/* WRMSR */
+		exit_info_1 = 1;
+		ghcb->save.rax = regs->ax;
+		ghcb_reg_set_valid(ghcb, VMSA_REG_RAX);
+		ghcb->save.rdx = regs->dx;
+		ghcb_reg_set_valid(ghcb, VMSA_REG_RDX);
+		/* Fallthrough */
+	case 0x32:	/* RDMSR */
+		ghcb->save.rcx = regs->cx;
+		ghcb_reg_set_valid(ghcb, VMSA_REG_RCX);
+		break;
+	default:
+		vmg_exit(ghcb, SVM_VMGEXIT_UNSUPPORTED_EVENT,
+			 SVM_EXIT_MSR, insn->opcode.bytes[0]);
+		return -EINVAL;
+	}
+
+	ret = vmg_exit(ghcb, SVM_EXIT_MSR, exit_info_1, 0);
+	if (ret)
+		return ret;
+
+	if (!exit_info_1) {
+		if (!ghcb_reg_is_valid(ghcb, VMSA_REG_RAX) ||
+		    !ghcb_reg_is_valid(ghcb, VMSA_REG_RDX)) {
+			vmg_exit(ghcb, SVM_VMGEXIT_UNSUPPORTED_EVENT,
+				 SVM_EXIT_MSR, 1);
+			return -EINVAL;
+		}
+		regs->ax = ghcb->save.rax;
+		regs->dx = ghcb->save.rdx;
+	}
+
+	return 0;
+}
+
 static int vmg_mmio_exec(struct ghcb *ghcb, unsigned long ghcb_pa,
 			 struct pt_regs *regs, struct insn *insn,
 			 unsigned int bytes, bool read)
@@ -635,6 +677,9 @@ static int sev_es_vc_exception(struct pt_regs *regs, long error_code)
 		break;
 	case SVM_EXIT_IOIO:
 		nae_exit = vmg_ioio;
+		break;
+	case SVM_EXIT_MSR:
+		nae_exit = vmg_msr;
 		break;
 	case SVM_EXIT_NPF:
 		nae_exit = vmg_mmio;
