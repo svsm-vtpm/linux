f7a59de87e9d4f7210d78472e1d7e57cb80c754b
Joerg Roedel <jroedel@suse.de>
2020-03-27 16:35:37 -0500

    x86/head/64: Rename start_cpu0
    For SEV-ES this entry point will be used for restarting APs after they
    have been offlined. Remove the '0' from the name to reflect that.
    
    Signed-off-by: Joerg Roedel <jroedel@suse.de>

--- arch/x86/kernel/smpboot.c
+++ arch/x86/kernel/smpboot.c
@@ -1717,7 +1717,7 @@ static inline void mwait_play_dead(void)
 		 * If NMI wants to wake up CPU0, start CPU0.
 		 */
 		if (wakeup_cpu0())
-			start_cpu0();
+			start_cpu();
 	}
 }
 
@@ -1732,7 +1732,7 @@ void hlt_play_dead(void)
 		 * If NMI wants to wake up CPU0, start CPU0.
 		 */
 		if (wakeup_cpu0())
-			start_cpu0();
+			start_cpu();
 	}
 }
 
