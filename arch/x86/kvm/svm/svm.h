ab81fc4f934cf01421d7bae329026020aebb4c80
Tom Lendacky <thomas.lendacky@amd.com>
2020-08-05 15:34:29 -0500

    KVM: SVM: Add support for booting APs for an SEV-ES guest
    Typically under KVM, an AP is booted using the INIT-SIPI-SIPI sequence,
    where the guest vCPU register state is updated and then the vCPU is VMRUN
    to begin execution of the AP. For an SEV-ES guest, this won't work because
    the guest register state is encrypted.
    
    Following the GHCB specification, the hypervisor must not alter the guest
    register state, so KVM must track an AP/vCPU boot. Should the guest want
    to park the AP, it must use the AP Reset Hold exit event in place of, for
    example, a HLT loop.
    
    First AP boot (first INIT-SIPI-SIPI sequence):
      Execute the AP (vCPU) as it was initialized and measured by the SEV-ES
      support. It is up to the guest to transfer control of the AP to the
      proper location.
    
    Subsequent AP boot:
      KVM will expect to receive an AP Reset Hold exit event indicating that
      the vCPU is being parked and will require an INIT-SIPI-SIPI sequence to
      awaken it. When the AP Reset Hold exit event is received, KVM will place
      the vCPU into a simulated HLT mode. Upon receiving the INIT-SIPI-SIPI
      sequence, KVM will make the vCPU runnable. It is again up to the guest
      to then transfer control of the AP to the proper location.
    
    The GHCB specification also requires the hypervisor to save the address of
    an AP Jump Table so that, for example, vCPUs that have been parked by UEFI
    can be started by the OS. Provide support for the AP Jump Table set/get
    exit code.
    
    Signed-off-by: Tom Lendacky <thomas.lendacky@amd.com>

--- arch/x86/kvm/svm/svm.h
+++ arch/x86/kvm/svm/svm.h
@@ -67,6 +67,7 @@ struct kvm_sev_info {
 	int fd;			/* SEV device fd */
 	unsigned long pages_locked; /* Number of pages locked */
 	struct list_head regions_list;  /* List of registered regions */
+	u64 ap_jump_table;	/* SEV-ES AP Jump Table address */
 };
 
 struct kvm_svm {
@@ -165,6 +166,7 @@ struct vcpu_svm {
 	struct vmcb_save_area *vmsa;
 	struct ghcb *ghcb;
 	struct kvm_host_map ghcb_map;
+	bool ap_hlt_loop;
 
 	/* SEV-ES scratch area support */
 	void *ghcb_sa;
@@ -565,6 +567,7 @@ void __init sev_hardware_setup(void);
 void sev_hardware_teardown(void);
 int sev_handle_vmgexit(struct vcpu_svm *svm);
 int sev_es_string_io(struct vcpu_svm *svm, int size, unsigned int port, int in);
+void sev_vcpu_deliver_sipi_vector(struct kvm_vcpu *vcpu, u8 vector);
 
 /* VMSA Accessor functions */
 
