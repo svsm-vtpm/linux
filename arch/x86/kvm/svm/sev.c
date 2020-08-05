9f2db19fc9b440531452d88071f81700a41a5595
Tom Lendacky <thomas.lendacky@amd.com>
2020-08-05 15:34:25 -0500

    KVM: SVM: Support port IO operations for an SEV-ES guest
    For an SEV-ES guest, port IO is performed to a shared (un-encrypted) page
    so that both the hypervisor and guest can read or write to it and each
    see the contents.
    
    For port IO operations, invoke SEV-ES specific routines that can complete
    the operation using common KVM port IO support.
    
    Signed-off-by: Tom Lendacky <thomas.lendacky@amd.com>

--- arch/x86/kvm/svm/sev.c
+++ arch/x86/kvm/svm/sev.c
@@ -1483,3 +1483,12 @@ int sev_handle_vmgexit(struct vcpu_svm *svm)
 
 	return ret;
 }
+
+int sev_es_string_io(struct vcpu_svm *svm, int size, unsigned int port, int in)
+{
+	if (!setup_vmgexit_scratch(svm, in, svm->vmcb->control.exit_info_2))
+		return -EINVAL;
+
+	return kvm_sev_es_string_io(&svm->vcpu, size, port,
+				    svm->ghcb_sa, svm->ghcb_sa_len, in);
+}
