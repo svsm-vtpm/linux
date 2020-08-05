2afc30b3b6c68a509524bb9ef86873f47f067f62
Tom Lendacky <thomas.lendacky@amd.com>
2020-08-05 15:34:27 -0500

    KVM: SVM: Add support for CR8 write traps for an SEV-ES guest
    For SEV-ES guests, the interception of control register write access
    is not recommended. Control register interception occurs prior to the
    control register being modified and the hypervisor is unable to modify
    the control register itself because the register is located in the
    encrypted register state.
    
    SEV-ES guests introduce new control register write traps. These traps
    provide intercept support of a control register write after the control
    register has been modified. The new control register value is provided in
    the VMCB EXITINFO1 field, allowing the hypervisor to track the setting
    of the guest control registers.
    
    Add support to track the value of the guest CR8 register using the control
    register write trap so that the hypervisor understands the guest operating
    mode.
    
    Signed-off-by: Tom Lendacky <thomas.lendacky@amd.com>

--- arch/x86/include/asm/kvm_host.h
+++ arch/x86/include/asm/kvm_host.h
@@ -1434,6 +1434,7 @@ int kvm_set_cr4(struct kvm_vcpu *vcpu, unsigned long cr4);
 int kvm_set_cr8(struct kvm_vcpu *vcpu, unsigned long cr8);
 int kvm_track_cr0(struct kvm_vcpu *vcpu, unsigned long cr0);
 int kvm_track_cr4(struct kvm_vcpu *vcpu, unsigned long cr4);
+int kvm_track_cr8(struct kvm_vcpu *vcpu, unsigned long cr8);
 int kvm_set_dr(struct kvm_vcpu *vcpu, int dr, unsigned long val);
 int kvm_get_dr(struct kvm_vcpu *vcpu, int dr, unsigned long *val);
 unsigned long kvm_get_cr8(struct kvm_vcpu *vcpu);
