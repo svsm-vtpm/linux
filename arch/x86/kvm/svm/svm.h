deb8cb00544bbebd3cc7ff5cf165aa7daa2fd716
Brijesh Singh <brijesh.singh@amd.com>
2021-04-25 10:12:16 -0500

    KVM: SVM: Increase the GHCB protocol version
    Now that KVM supports version 2 of the GHCB specification, bump the
    maximum supported protocol version.
    
    Signed-off-by: Brijesh Singh <brijesh.singh@amd.com>

--- arch/x86/kvm/svm/svm.h
+++ arch/x86/kvm/svm/svm.h
@@ -527,7 +527,7 @@ void svm_vcpu_unblocking(struct kvm_vcpu *vcpu);
 
 /* sev.c */
 
-#define GHCB_VERSION_MAX	1ULL
+#define GHCB_VERSION_MAX	2ULL
 #define GHCB_VERSION_MIN	1ULL
 
 #define GHCB_HV_FEATURES_SUPPORTED	0
