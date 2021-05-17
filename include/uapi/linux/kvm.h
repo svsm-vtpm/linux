7dcb7c900179380a35cf39bc802162b41ceb8d1b
Brijesh Singh <brijesh.singh@amd.com>
2021-05-17 15:25:11 -0500

    KVM: SVM: Add KVM_SEV_SNP_LAUNCH_FINISH command
    The KVM_SEV_SNP_LAUNCH_FINISH finalize the cryptographic digest and stores
    it as the measurement of the guest at launch.
    
    While finalizing the launch flow, it also issues the LAUNCH_UPDATE command
    to encrypt the VMSA pages.
    
    Signed-off-by: Brijesh Singh <brijesh.singh@amd.com>

--- include/uapi/linux/kvm.h
+++ include/uapi/linux/kvm.h
@@ -1682,6 +1682,7 @@ enum sev_cmd_id {
 	KVM_SEV_SNP_INIT = 256,
 	KVM_SEV_SNP_LAUNCH_START,
 	KVM_SEV_SNP_LAUNCH_UPDATE,
+	KVM_SEV_SNP_LAUNCH_FINISH,
 
 	KVM_SEV_NR_MAX,
 };
@@ -1806,6 +1807,18 @@ struct kvm_sev_snp_launch_update {
 	__u8 vmpl3_perms;
 	__u8 vmpl2_perms;
 	__u8 vmpl1_perms;
+};
+
+#define KVM_SEV_SNP_ID_BLOCK_SIZE	96
+#define KVM_SEV_SNP_ID_AUTH_SIZE	4096
+#define KVM_SEV_SNP_FINISH_DATA_SIZE	32
+
+struct kvm_sev_snp_launch_finish {
+	__u64 id_block_uaddr;
+	__u64 id_auth_uaddr;
+	__u8 id_block_en;
+	__u8 auth_key_en;
+	__u8 host_data[KVM_SEV_SNP_FINISH_DATA_SIZE];
 };
 
 #define KVM_DEV_ASSIGN_ENABLE_IOMMU	(1 << 0)
