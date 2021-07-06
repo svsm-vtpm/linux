af010c362db65b4f30accf6eddf7296899689f56
Brijesh Singh <brijesh.singh@amd.com>
2021-07-06 18:41:57 -0500

    x86/compressed: Add helper for validating pages in the decompression stage
    Many of the integrity guarantees of SEV-SNP are enforced through the
    Reverse Map Table (RMP). Each RMP entry contains the GPA at which a
    particular page of DRAM should be mapped. The VMs can request the
    hypervisor to add pages in the RMP table via the Page State Change VMGEXIT
    defined in the GHCB specification. Inside each RMP entry is a Validated
    flag; this flag is automatically cleared to 0 by the CPU hardware when a
    new RMP entry is created for a guest. Each VM page can be either
    validated or invalidated, as indicated by the Validated flag in the RMP
    entry. Memory access to a private page that is not validated generates
    a #VC. A VM must use PVALIDATE instruction to validate the private page
    before using it.
    
    To maintain the security guarantee of SEV-SNP guests, when transitioning
    pages from private to shared, the guest must invalidate the pages before
    asking the hypervisor to change the page state to shared in the RMP table.
    
    After the pages are mapped private in the page table, the guest must issue
    a page state change VMGEXIT to make the pages private in the RMP table and
    validate it.
    
    On boot, BIOS should have validated the entire system memory. During
    the kernel decompression stage, the VC handler uses the
    set_memory_decrypted() to make the GHCB page shared (i.e clear encryption
    attribute). And while exiting from the decompression, it calls the
    set_page_encrypted() to make the page private.
    
    Add sev_snp_set_page_{private,shared}() helper that is used by the
    set_memory_{decrypt,encrypt}() to change the page state in the RMP table.
    
    Signed-off-by: Brijesh Singh <brijesh.singh@amd.com>

--- arch/x86/include/asm/sev-common.h
+++ arch/x86/include/asm/sev-common.h
@@ -45,6 +45,23 @@
 		(((unsigned long)reg & GHCB_MSR_CPUID_REG_MASK) << GHCB_MSR_CPUID_REG_POS) | \
 		(((unsigned long)fn) << GHCB_MSR_CPUID_FUNC_POS))
 
+/* SNP Page State Change */
+#define GHCB_MSR_PSC_REQ		0x014
+#define SNP_PAGE_STATE_PRIVATE		1
+#define SNP_PAGE_STATE_SHARED		2
+#define GHCB_MSR_PSC_GFN_POS		12
+#define GHCB_MSR_PSC_GFN_MASK		GENMASK_ULL(39, 0)
+#define GHCB_MSR_PSC_OP_POS		52
+#define GHCB_MSR_PSC_OP_MASK		0xf
+#define GHCB_MSR_PSC_REQ_GFN(gfn, op)	\
+	(((unsigned long)((op) & GHCB_MSR_PSC_OP_MASK) << GHCB_MSR_PSC_OP_POS) | \
+	((unsigned long)((gfn) & GHCB_MSR_PSC_GFN_MASK) << GHCB_MSR_PSC_GFN_POS) | \
+	GHCB_MSR_PSC_REQ)
+
+#define GHCB_MSR_PSC_RESP		0x015
+#define GHCB_MSR_PSC_ERROR_POS		32
+#define GHCB_MSR_PSC_RESP_VAL(val)	((val) >> GHCB_MSR_PSC_ERROR_POS)
+
 /* GHCB Hypervisor Feature Request */
 #define GHCB_MSR_HV_FT_REQ	0x080
 #define GHCB_MSR_HV_FT_RESP	0x081
