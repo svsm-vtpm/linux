8b7148f2fae8c223db8a9492ded603ba69c59bcb
Brijesh Singh <brijesh.singh@amd.com>
2021-08-03 06:42:01 -0500

    KVM: x86: Export the kvm_zap_gfn_range() for the SNP use
    While resolving the RMP page fault, we may run into cases where the page
    level between the RMP entry and TDP does not match and the 2M RMP entry
    must be split into 4K RMP entries. Or a 2M TDP page need to be broken
    into multiple of 4K pages.
    
    To keep the RMP and TDP page level in sync, we will zap the gfn range
    after splitting the pages in the RMP entry. The zap should force the
    TDP to gets rebuilt with the new page level.
    
    Signed-off-by: Brijesh Singh <brijesh.singh@amd.com>

--- arch/x86/kvm/mmu/mmu.c
+++ arch/x86/kvm/mmu/mmu.c
@@ -5748,6 +5748,7 @@ restart:
 
 	return need_tlb_flush;
 }
+EXPORT_SYMBOL_GPL(kvm_zap_gfn_range);
 
 void kvm_mmu_zap_collapsible_sptes(struct kvm *kvm,
 				   const struct kvm_memory_slot *memslot)
