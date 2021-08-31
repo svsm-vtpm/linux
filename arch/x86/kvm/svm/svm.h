74d8359ae6501649193bbd627277b2093b71ada5
Tom Lendacky <thomas.lendacky@amd.com>
2021-08-31 08:45:02 -0500

    KVM: SVM: Create a separate mapping for the SEV-ES save area
    The save area for SEV-ES/SEV-SNP guests, as used by the hardware, is
    different from the save area of a non SEV-ES/SEV-SNP guest.
    
    This is the first step in defining the multiple save areas to keep them
    separate and ensuring proper operation amongst the different types of
    guests. Create an SEV-ES/SEV-SNP save area and adjust usage to the new
    save area definition where needed.
    
    Signed-off-by: Tom Lendacky <thomas.lendacky@amd.com>
    Signed-off-by: Brijesh Singh <brijesh.singh@amd.com>

--- arch/x86/kvm/svm/svm.h
+++ arch/x86/kvm/svm/svm.h
@@ -184,7 +184,7 @@ struct vcpu_svm {
 	} shadow_msr_intercept;
 
 	/* SEV-ES support */
-	struct vmcb_save_area *vmsa;
+	struct sev_es_save_area *vmsa;
 	struct ghcb *ghcb;
 	struct kvm_host_map ghcb_map;
 	bool received_first_sipi;
