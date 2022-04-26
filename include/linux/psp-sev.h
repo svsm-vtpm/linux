b0f1e20b35b2c5ff9bbc92d5eb6e26fd8b5790f1
Brijesh Singh <brijesh.singh@amd.com>
2022-04-26 18:01:31 +0000

    crypto: ccp: Handle the legacy TMR allocation when SNP is enabled
    The behavior and requirement for the SEV-legacy command is altered when
    the SNP firmware is in the INIT state. See SEV-SNP firmware specification
    for more details.
    
    Allocate the Trusted Memory Region (TMR) as a 2mb sized/aligned region
    when SNP is enabled to satify new requirements for the SNP. Continue
    allocating a 1mb region for !SNP configuration.
    
    While at it, provide API that can be used by others to allocate a page
    that can be used by the firmware. The immediate user for this API will
    be the KVM driver. The KVM driver to need to allocate a firmware context
    page during the guest creation. The context page need to be updated
    by the firmware. See the SEV-SNP specification for further details.
    
    Signed-off-by: Brijesh Singh <brijesh.singh@amd.com>

--- include/linux/psp-sev.h
+++ include/linux/psp-sev.h
@@ -12,6 +12,8 @@
 #ifndef __PSP_SEV_H__
 #define __PSP_SEV_H__
 
+#include <linux/sev.h>
+
 #include <uapi/linux/psp-sev.h>
 
 #ifdef CONFIG_X86
@@ -940,6 +942,8 @@ int snp_guest_page_reclaim(struct sev_data_snp_page_reclaim *data, int *error);
 int snp_guest_dbg_decrypt(struct sev_data_snp_dbg *data, int *error);
 
 void *psp_copy_user_blob(u64 uaddr, u32 len);
+void *snp_alloc_firmware_page(gfp_t mask);
+void snp_free_firmware_page(void *addr);
 
 #else	/* !CONFIG_CRYPTO_DEV_SP_PSP */
 
@@ -980,6 +984,13 @@ static inline int snp_guest_dbg_decrypt(struct sev_data_snp_dbg *data, int *erro
 {
 	return -ENODEV;
 }
+
+static inline void *snp_alloc_firmware_page(gfp_t mask)
+{
+	return NULL;
+}
+
+static inline void snp_free_firmware_page(void *addr) { }
 
 #endif	/* CONFIG_CRYPTO_DEV_SP_PSP */
 
