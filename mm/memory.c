c0f7c356de3e4ace064e1ec02928e4e7b19d6664
Brijesh Singh <brijesh.singh@amd.com>
2022-04-26 17:46:18 +0000

    x86/fault: Add support to handle the RMP fault for user address
    When SEV-SNP is enabled globally, a write from the host goes through the
    RMP check. When the host writes to pages, hardware checks the following
    conditions at the end of page walk:
    
    1. Assigned bit in the RMP table is zero (i.e page is shared).
    2. If the page table entry that gives the sPA indicates that the target
       page size is a large page, then all RMP entries for the 4KB
       constituting pages of the target must have the assigned bit 0.
    3. Immutable bit in the RMP table is not zero.
    
    The hardware will raise page fault if one of the above conditions is not
    met. Try resolving the fault instead of taking fault again and again. If
    the host attempts to write to the guest private memory then send the
    SIGBUS signal to kill the process. If the page level between the host and
    RMP entry does not match, then split the address to keep the RMP and host
    page levels in sync.
    
    Signed-off-by: Brijesh Singh <brijesh.singh@amd.com>

--- mm/memory.c
+++ mm/memory.c
@@ -4968,6 +4968,15 @@ unlock:
 	return 0;
 }
 
+static int handle_split_page_fault(struct vm_fault *vmf)
+{
+	if (!IS_ENABLED(CONFIG_AMD_MEM_ENCRYPT))
+		return VM_FAULT_SIGBUS;
+
+	__split_huge_pmd(vmf->vma, vmf->pmd, vmf->address, false, NULL);
+	return 0;
+}
+
 /*
  * By the time we get here, we already hold the mm semaphore
  *
@@ -5050,6 +5059,10 @@ retry_pud:
 				pmd_migration_entry_wait(mm, vmf.pmd);
 			return 0;
 		}
+
+		if (flags & FAULT_FLAG_PAGE_SPLIT)
+			return handle_split_page_fault(&vmf);
+
 		if (pmd_trans_huge(vmf.orig_pmd) || pmd_devmap(vmf.orig_pmd)) {
 			if (pmd_protnone(vmf.orig_pmd) && vma_is_accessible(vma))
 				return do_huge_pmd_numa_page(&vmf);
