8626ff04faf35ba35e109edaa32d424da15dd550
Brijesh Singh <brijesh.singh@amd.com>
2022-04-26 19:11:25 +0000

    KVM: SVM: Introduce ops for the post gfn map and unmap
    When SEV-SNP is enabled in the guest VM, the guest memory pages can
    either be a private or shared. A write from the hypervisor goes through
    the RMP checks. If hardware sees that hypervisor is attempting to write
    to a guest private page, then it triggers an RMP violation #PF.
    
    To avoid the RMP violation with GHCB pages, added new post_{map,unmap}_gfn
    functions to verify if its safe to map GHCB pages.  Uses a spinlock to
    protect against the page state change for existing mapped pages.
    
    Need to add generic post_{map,unmap}_gfn() ops that can be used to verify
    that its safe to map a given guest page in the hypervisor.
    
    This patch will need to be revisited later after consensus is reached on
    how to manage guest private memory as probably UPM private memslots will
    be able to handle this page state change more gracefully.
    
    Signed-off-by: Brijesh Singh <brijesh.singh@amd.com>
    Signed-off by: Ashish Kalra <ashish.kalra@amd.com>

--- arch/x86/kvm/svm/svm.h
+++ arch/x86/kvm/svm/svm.h
@@ -79,19 +79,25 @@ struct kvm_sev_info {
 	bool active;		/* SEV enabled guest */
 	bool es_active;		/* SEV-ES enabled guest */
 	bool snp_active;	/* SEV-SNP enabled guest */
+
 	unsigned int asid;	/* ASID used for this guest */
 	unsigned int handle;	/* SEV firmware handle */
 	int fd;			/* SEV device fd */
+
 	unsigned long pages_locked; /* Number of pages locked */
 	struct list_head regions_list;  /* List of registered regions */
+
 	u64 ap_jump_table;	/* SEV-ES AP Jump Table address */
+
 	struct kvm *enc_context_owner; /* Owner of copied encryption context */
 	struct list_head mirror_vms; /* List of VMs mirroring */
 	struct list_head mirror_entry; /* Use as a list entry of mirrors */
 	struct misc_cg *misc_cg; /* For misc cgroup accounting */
 	atomic_t migration_in_progress;
+
 	u64 snp_init_flags;
 	void *snp_context;      /* SNP guest context page */
+	spinlock_t psc_lock;
 };
 
 struct kvm_svm {
@@ -702,6 +708,11 @@ void sev_es_prepare_switch_to_guest(struct sev_es_save_area *hostsa);
 void sev_es_unmap_ghcb(struct vcpu_svm *svm);
 struct page *snp_safe_alloc_page(struct kvm_vcpu *vcpu);
 void sev_rmp_page_level_adjust(struct kvm *kvm, kvm_pfn_t pfn, int *level);
+int sev_post_map_gfn(struct kvm *kvm, gfn_t gfn, kvm_pfn_t pfn);
+void sev_post_unmap_gfn(struct kvm *kvm, gfn_t gfn, kvm_pfn_t pfn);
+void handle_rmp_page_fault(struct kvm_vcpu *vcpu, gpa_t gpa, u64 error_code);
+void sev_snp_init_protected_guest_state(struct kvm_vcpu *vcpu);
+int sev_snp_update_protected_guest_state(struct kvm_vcpu *vcpu);
 
 /* vmenter.S */
 
