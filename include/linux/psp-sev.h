87a0481526e66ddd5f6192cbb43a50708aee2883
Brijesh Singh <brijesh.singh@amd.com>
2022-04-26 17:57:14 +0000

    crypto: ccp: Add support to initialize the AMD-SP for SEV-SNP
    Before SNP VMs can be launched, the platform must be appropriately
    configured and initialized. Platform initialization is accomplished via
    the SNP_INIT command. Make sure to do a WBINVD and issue DF_FLUSH command
    to prepare for the first SNP guest launch after INIT.
    
    Signed-off-by: Brijesh Singh <brijesh.singh@amd.com>
    Signed-off by: Ashish Kalra <ashish.kalra@amd.com>

--- include/linux/psp-sev.h
+++ include/linux/psp-sev.h
@@ -770,6 +770,20 @@ struct sev_data_snp_init_ex {
 int sev_platform_init(int *error);
 
 /**
+ * sev_snp_init - perform SEV SNP_INIT command
+ *
+ * @error: SEV command return code
+ *
+ * Returns:
+ * 0 if the SEV successfully processed the command
+ * -%ENODEV    if the SEV device is not available
+ * -%ENOTSUPP  if the SEV does not support SEV
+ * -%ETIMEDOUT if the SEV command timed out
+ * -%EIO       if the SEV returned a non-zero return code
+ */
+int sev_snp_init(int *error);
+
+/**
  * sev_platform_status - perform SEV PLATFORM_STATUS command
  *
  * @status: sev_user_data_status structure to be processed
@@ -875,6 +889,8 @@ static inline int
 sev_platform_status(struct sev_user_data_status *status, int *error) { return -ENODEV; }
 
 static inline int sev_platform_init(int *error) { return -ENODEV; }
+
+static inline int sev_snp_init(int *error) { return -ENODEV; }
 
 static inline int
 sev_guest_deactivate(struct sev_data_deactivate *data, int *error) { return -ENODEV; }
